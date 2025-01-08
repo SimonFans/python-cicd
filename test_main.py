2df52d69c7d8d061e2bb58ac59d2a2093cff9239
Simon Zhao <ximengzhao1220@gmail.com>
2025-01-07 22:02:35 -0800

    add new tests

--- test_main.py
+++ test_main.py
@@ -1,11 +1,16 @@
 import unittest
-from main import add
+from main import add, minus
 
 class TestMain(unittest.TestCase):
     def test_add(self):
         self.assertEqual(add(2, 3), 5)
         self.assertEqual(add(-1, 3), 2)
         self.assertEqual(add(0, 0), 0)
+
+    def test_minus(self):
+        self.assertEqual(minus(2, 3), -1)
+        self.assertEqual(minus(1, 3), -2)
+        self.assertEqual(minus(0, 0), 0)
 
 if __name__ == "__main__":
     unittest.main()
